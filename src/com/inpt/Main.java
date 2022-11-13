cc81e3c9fa4890e912add9129ad0bc38074ac592
AbdoRty <117866341+AbdoRty@users.noreply.github.com>
2022-11-13 13:56:25 +0100

    Update Main.java

--- src/com/inpt/Main.java
+++ src/com/inpt/Main.java
@@ -2,6 +2,6 @@ package com.inpt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word abderrahim");
+        System.out.println("Hello word abderrahim !!");
     }
 }
